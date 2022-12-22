25823e03ad7e4fdafc18fb1911c444dc1142d47d
Diego Fan <diegoeco.fan@gmail.com>
2022-12-22 15:31:45 -0300

    Commit do README na main

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.9
-
-RUN apt-get update
-RUN mkdir app
-COPY requirements.txt /app
-
-RUN pip install --upgrade pip \
-    pip install -r /app/requirements.txt
--- src/queries/train_test_split.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    *,
-    CASE
-    WHEN ABS(MOD(FARM_FINGERPRINT(TO_JSON_STRING(i)), 10)) < 8 THEN 'train'
-    ELSE 'test'
-    END data_split
-FROM
-    `{{source_table}}` as i
